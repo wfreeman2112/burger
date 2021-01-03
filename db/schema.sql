4141b9a25423da39a6bb5242013af1467712c541
Wendi Freeman <wfreeman2112@gmail.com>
2021-01-03 09:40:10 -0600

    creating MySQL table

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+-- Create the `burgers_db`.
+DROP DATABASE IF EXISTS burgers_DB;
+CREATE DATABASE  burgers_DB;
+-- Switch to or use the `burgers_db`.
+USE burgers_DB;
+-- Create a `burgers` table with these fields:
+-- **id**: an auto incrementing int that serves as the primary key.
+-- **burger_name**: a string.
+-- **devoured**: a boolean.
+
+CREATE TABLE burgers (
+id INTEGER AUTO_INCREMENT NOT Null,
+burger name VARCHAR(30),
+devoured BOOLEAN,
+PRIMARY KEY (id)
+);
