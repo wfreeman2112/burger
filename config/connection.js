47b9896434e25bddb79977fa72c0ea5c3ace8b1f
Wendi Freeman <wfreeman2112@gmail.com>
2021-01-10 17:15:19 -0600

    fixing connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
-  var connection = mysql.createConnection(process.env.JAWSDB_URL)
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   const connection = mysql.createConnection({
     host: "localhost",
@@ -11,25 +11,8 @@ if (process.env.JAWSDB_URL) {
     password: "password1",
     database: "burgers_DB",
   });
-}
- 
- 
-//db.json to store and retrieve notes
- 
-
-//make connection to MySQL
-const connection = mysql.createConnection({
-  host: "localhost",
-  PORT: 3306,
-  user: "root",
-  password: "password1",
-  database: "burgers_DB",
-});
-
-connection.connect(function (err) {
+}connection.connect(function (err) {
   if (err) throw err;
-
 });
 
-module.exports = connection
-
+module.exports = connection;
